4b245544a63118cea4269da7fd9d7538e12e6a7a
JasonQuantrill <91751222+JasonQuantrill@users.noreply.github.com>
2021-12-31 13:15:30 -0500

    Update class.py

--- class.py
+++ class.py
@@ -5,118 +5,149 @@ import datetime
 #datetime allows the program to read what time and day of the week it is
 
 
-def openClass(date):
+class Course:
+    def __init__(self,
+                lecture1_day, lecture1_time, lecture1_link,
+                lecture2_day, lecture2_time, lecture2_link,
+                lab_day, lab_time, lab_link):
+        self.lecture1_day = lecture1_day
+        self.lecture1_time = lecture1_time
+        self.lecture1_link = lecture1_link
+        self.lecture2_day = lecture2_day
+        self.lecture2_time = lecture2_time
+        self.lecture2_link = lecture2_link
+        self.lab_day = lab_day
+        self.lab_time = lab_time
+        self.lab_link = lab_link
+
+def openClass(date,courses):
+
+    for course in courses:
+        if course.lecture1_day == date.weekday():
+            if (course.lecture1_time == date.timetuple()[3] and date.timetuple()[4] < 45) \
+               or (course.lecture1_time == (date.timetuple()[3] - 1) and date.timetuple()[4] > 45):
+                webbrowser.open(course.lecture1_link)
+        elif course.lecture2_day == date.weekday():
+            if (course.lecture2_time == date.timetuple()[3] and date.timetuple()[4] < 45) \
+               or (course.lecture2_time == (date.timetuple()[3] - 1) and date.timetuple()[4] > 45):
+                webbrowser.open(course.lecture2_link)
+        elif course.lab_day == date.weekday():
+            if (course.lab_time == date.timetuple()[3] and date.timetuple()[4] < 45) \
+               or (course.lab_time == (date.timetuple()[3] - 1) and date.timetuple()[4] > 45):
+                webbrowser.open(course.lab_link)
+        
+
     
-    ########
-    #MONDAY#
-    ########
-    if date.weekday() == 0:
-        ###########################w
-        # MTH 714 LECTURE 11AM - 2PM
-        #
-        if (10 == date.timetuple()[3] and 45 < date.timetuple()[4]) or (11 == date.timetuple()[3] and 30 > date.timetuple()[4]):
-            webbrowser.open('https://ryerson.zoom.us/j/99524332807?pwd=MjJyRUR3enVIM0VySXRwd3l1dFpaQT09')
-            
-        ##########################
-        #CPS 305 LECTURE 2PM - 3PM
-        #
-        elif (13 == date.timetuple()[3] and 45 < date.timetuple()[4]) or (14 == date.timetuple()[3] and 30 > date.timetuple()[4]):
-            webbrowser.open('https://courses.ryerson.ca/d2l/le/content/543807/viewContent/3901425/View')
-            
-        
+courses = []
 
-    #########
-    #TUESDAY#
-    #########
-    elif date.weekday() == 1:
-        
-        #######################w
-        #MTH 108 LECTURE 8AM - 10AM
-        #
-        if (7 == date.timetuple()[3] and 45 < date.timetuple()[4]) or (8 == date.timetuple()[3] and 30 > date.timetuple()[4]):
-            webbrowser.open('https://ryerson.zoom.us/j/98836755855?pwd=RlpkSkd2QVJVdlhtcWJESk9Zb0dlQT09')
-            
-        ############################
-        #CPS 393 LECTURE 11AM - 1PM
-        #
-        elif (10 == date.timetuple()[3] and 45 < date.timetuple()[4]) or (11 == date.timetuple()[3] and 30 > date.timetuple()[4]):
-            webbrowser.open('https://ryerson.zoom.us/j/98869774311?pwd=cG1qQ1NrdjRhUGtZQXBiNWZFOWpsUT09')
-            
-        
+#################
+#USER INPUT BEGIN
+#################
 
-    ###########
-    #WEDNESDAY#
-    ###########
-    elif date.weekday() == 2:
-        
-        ###########################w
-        #MTH 714 TUTORIAL 10AM - 11AM
-        #
-        if date.timetuple()[3] >= 9 and date.timetuple()[3] <= 11:
-            webbrowser.open('https://ryerson.zoom.us/j/98391128075?pwd=eHVGeFBQYlIzYTgwSzczdHJhTUlBdz09')
+#######
+#MTH108
+#
+lecture1_day = 0    #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lecture1_time = 13  #Round to nearest hour; use 24 hour clock
+lecture1_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
 
-        ###########################w
-        #CPS 305 LECTURE 12PM - 2PM
-        #
-        elif (11 == date.timetuple()[3] and 45 < date.timetuple()[4]) or (12 == date.timetuple()[3] and 30 > date.timetuple()[4]):
-            webbrowser.open('https://ryerson.zoom.us/j/93275255781?pwd=M1o4dXdHOXBHNUVlQnUzSkd6djJqdz09#success')
+lecture2_day = 0    #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lecture2_time = 13  #Round to nearest hour; use 24 hour clock
+lecture2_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
 
-        ###########################w
-        #CPS 393 LAB 2PM - 3PM
-        #
-        elif (13 == date.timetuple()[3] and 45 < date.timetuple()[4]) or (14 == date.timetuple()[3] and 30 > date.timetuple()[4]):
-            webbrowser.open('https://ryerson.zoom.us/j/98610752341?pwd=RVFqcVJ6N2RsdWZCcmY1YUd2TVZVZz09')
+lab_day = 0         #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lab_time = 13       #Round to nearest hour; use 24 hour clock
+lab_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
+
+course1 = Course(lecture1_day, lecture1_time, lecture1_link,
+                lecture2_day, lecture2_time, lecture2_link,
+                lab_day, lab_time, lab_link)
+courses.append(course1)
+
+#######
+#MTH714
+#
+lecture1_day = 0    #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lecture1_time = 13  #Round to nearest hour; use 24 hour clock
+lecture1_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
+
+lecture2_day = 0    #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lecture2_time = 13  #Round to nearest hour; use 24 hour clock
+lecture2_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
+
+lab_day = 0         #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lab_time = 13       #Round to nearest hour; use 24 hour clock
+lab_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
+
+course2 = Course(lecture1_day, lecture1_time, lecture1_link,
+                lecture2_day, lecture2_time, lecture2_link,
+                lab_day, lab_time, lab_link)
+courses.append(course2)
+
+#######
+#CPS305
+#
+lecture1_day = 0    #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lecture1_time = 13  #Round to nearest hour; use 24 hour clock
+lecture1_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
+
+lecture2_day = 0    #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lecture2_time = 13  #Round to nearest hour; use 24 hour clock
+lecture2_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
+
+lab_day = 0         #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lab_time = 13       #Round to nearest hour; use 24 hour clock
+lab_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
+
+course3 = Course(lecture1_day, lecture1_time, lecture1_link,
+                lecture2_day, lecture2_time, lecture2_link,
+                lab_day, lab_time, lab_link)
+courses.append(course3)
+
+#######
+#CPS393
+#
+lecture1_day = 4    #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lecture1_time = 13  #Round to nearest hour; use 24 hour clock
+lecture1_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
+
+lecture2_day = 0    #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lecture2_time = 13  #Round to nearest hour; use 24 hour clock
+lecture2_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
+
+lab_day = 0         #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lab_time = 13       #Round to nearest hour; use 24 hour clock
+lab_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
+
+course4 = Course(lecture1_day, lecture1_time, lecture1_link,
+                lecture2_day, lecture2_time, lecture2_link,
+                lab_day, lab_time, lab_link)
+courses.append(course4)
+
+#######
+#CMN300
+#
+lecture1_day = 0    #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lecture1_time = 13  #Round to nearest hour; use 24 hour clock
+lecture1_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
+
+lecture2_day = 0    #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lecture2_time = 13  #Round to nearest hour; use 24 hour clock
+lecture2_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
+
+lab_day = 0         #0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday
+lab_time = 13       #Round to nearest hour; use 24 hour clock
+lab_link = 'https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09' #link to zoom meeting
+
+course5 = Course(lecture1_day, lecture1_time, lecture1_link,
+                lecture2_day, lecture2_time, lecture2_link,
+                lab_day, lab_time, lab_link)
+courses.append(course5)
+
+###############
+#USER INPUT END
+###############
 
 
-
-            
-    ##########
-    #THURSDAY#
-    ##########
-    elif date.weekday() == 3:
-
-        ############################w
-        #MTH 108 LAB 10AM - 11AM
-        #
-        if (9 == date.timetuple()[3] and 45 < date.timetuple()[4]) or (10 == date.timetuple()[3] and 30 > date.timetuple()[4]):
-            webbrowser.open('https://ryerson.zoom.us/j/98283811244?pwd=VSsvbUFDRlNpQzFoL3VpMGY0U0tHZz09')
-
-        ############################w
-        #CPS 393 LECTURE 11AM - 12PM
-        #
-        elif (10 == date.timetuple()[3] and 45 < date.timetuple()[4]) or (11 == date.timetuple()[3] and 30 > date.timetuple()[4]):
-            webbrowser.open('https://ryerson.zoom.us/j/98869774311?pwd=cG1qQ1NrdjRhUGtZQXBiNWZFOWpsUT09')
-
-        ######################w
-        #CPS 305 LAB 2PM - 3PM
-        #
-        elif (13 == date.timetuple()[3] and 45 < date.timetuple()[4]) or (14 == date.timetuple()[3] and 40 > date.timetuple()[4]):
-            webbrowser.open('https://ryerson.zoom.us/j/92551508700?pwd=V3ArWEQ4eHlReTVMOUVncFY2M25WZz09')
-
-            
-    ########    
-    #FRIDAY#
-    ########
-    elif date.weekday() == 4:
-
-        ###########################w
-        #MTH 108 LECTURE 8AM - 10AM
-        #
-        if (7 == date.timetuple()[3] and 45 < date.timetuple()[4]) or (8 == date.timetuple()[3] and 59 > date.timetuple()[4]):
-            webbrowser.open('https://ryerson.zoom.us/j/99504411891?pwd=cUUxbzhvTW84bGlEaGh5cnpwMnNIdz09')
-
-        ############################
-        #CPS 310 LECTURE 10AM - 12PM
-        #
-        elif (9 == date.timetuple()[3] and 45 < date.timetuple()[4]) or (10 == date.timetuple()[3] and 30 > date.timetuple()[4]):
-            webbrowser.open('https://ryerson.zoom.us/j/94195550253?pwd=S2F3ZlBmeEhBY3VITm5uakJPSUZEdz09')
-            
-
-
-date = datetime.datetime.today()
-#assigns the date to the variable so it can be interacted with
-#format is a tuple with order (year, month, day, hour, minute, second)
-#Day --> Monday=0 , Friday=4 , Sunday=6
-
-openClass(date)
-#calls openClass() function to produce the useful output
+date = datetime.datetime.today() #format is a tuple with order: (year, month, day, hour, minute, second)
+openClass(date, courses)
